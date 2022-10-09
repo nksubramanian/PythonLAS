60314a6a86eaf67eb51889c929efb0d2052ce915
subramanian.kumarappan <subramanian.nk@live.com>
2022-10-09 15:53:59 +0530

    file saving done

--- pythonApp/app.py
+++ pythonApp/app.py
@@ -1,9 +1,13 @@
+import os
+
 from flask import Flask, render_template, request
 import random
 import lasio as lasio
+from werkzeug.utils import secure_filename
 
+Upload = 'static/upload'
 app = Flask(__name__)
-
+app.config['uploadFolder'] = Upload
 # list of cat images
 images = [
     "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26388-1381844103-11.gif",
@@ -32,17 +36,31 @@ def getfile():
 
 @app.route("/getwells", methods=["GET"])
 def getwells():
-    content = request.files['file'].read()
-    las = lasio.open_file(content)
-    #las = lasio.read(content)
-    return str(las.well)
+    file = request.files['file']
+    file.save(secure_filename(file.filename))
+    filename = file.filename
+    file_name = request.form[filename]
+    with open(file_name, 'r') as f:
+        file_content = f.read()
+    return file_content
+
+
+@app.route("/upload", methods = ['POST'])
+def upload():
+    file = request.files['file']
+    filename = file.filename
+    print(filename)
+    file.save(file.filename)
+    print(os.path)
+    return filename
+
+
 
 @app.route("/getfilename", methods=["POST"])
 def getfilename():
     file = request.files['file']
-    #with open(file_name, 'r') as f:
-    #    file_content = f.read()
     return str(file.filename)
+
 
 
 if __name__ == "__main__":
